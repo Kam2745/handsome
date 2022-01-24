b22d21f0d1012a66a4d6b34bd0a1519a2175bb66
Kamrul2745 <Kamrul_aiub2003@yahoo.com>
2022-01-23 23:17:49 -0600

    love

---
+++ src/main/java/kamrul/hasan/handsome/App.java
@@ -0,0 +1,13 @@
+package kamrul.hasan.handsome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
